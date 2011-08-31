e297c79b6414878108563350cc823d3fee812f45
bharatsingh430@gmail.com <bharatsingh430@gmail.com@c8676eb3-1d9d-3327-d37b-6325948daf39>
2011-08-31 12:42:15 +0000

    Modified Adder to include High level CLA logic

--- trunk/csdiitm-assignment2/claadder.v
+++ trunk/csdiitm-assignment2/claadder.v
@@ -1,8 +1,8 @@
 `include "stimulus.v"
 
-module cla4(out, cout, in1, in2, c0);
+module cla4(out, cout, g, p, in1, in2, c0);
    output [3:0] out;
-   output 	cout;
+   output 	cout, g, p;
    input [3:0] in1, in2;
    input c0;
 
@@ -46,8 +46,62 @@ module cla4(out, cout, in1, in2, c0);
    xor(out[1], p1, c1);
    xor(out[2], p2, c2);
    xor(out[3], p3, c3);
-   
+
+   and(p, p3, p2, p1, p0);
+   or(g, g3, g2p3, g1p23, g0p13);
 endmodule // cla4
+
+
+module hierarchial_cla(out, cout, in1, in2, as, clk);
+   output [15:0] out;
+   output 	cout;
+   input [15:0] in1, in2;
+   input as, clk;
+
+   wire  c4, c8, c12;
+   wire  cout0, cout1, cout2;
+   wire  g0, p0, g1, p1, g2, p2, g3, p3;
+   wire  p01, p02, p03, p12, p13, p23;
+   wire cp0, cp01, cp02, cp03;
+   wire g0p1, g1p2, g0p12, g2p3, g1p23, g0p13;
+   wire [15:0] 	 in2m;   
+   
+   xor(in2m[0], in2[0], as);
+   xor(in2m[1], in2[1], as);
+   xor(in2m[2], in2[2], as);
+   xor(in2m[3], in2[3], as);
+   xor(in2m[4], in2[4], as);
+   xor(in2m[5], in2[5], as);
+   xor(in2m[6], in2[6], as);
+   xor(in2m[7], in2[7], as);
+   xor(in2m[8], in2[8], as);
+   xor(in2m[9], in2[9], as);
+   xor(in2m[10], in2[10], as);
+   xor(in2m[11], in2[11], as);
+   xor(in2m[12], in2[12], as);
+   xor(in2m[13], in2[13], as);
+   xor(in2m[14], in2[14], as);
+   xor(in2m[15], in2[15], as);
+
+   cla4 CLA1(out[3:0], cout0, g0, p0, in1[3:0], in2m[3:0], as);
+   and(cp0, as, p0);
+   or(c4, g0, cp0);
+   cla4 CLA2(out[7:4], cout1, g1, p1, in1[7:4], in2m[7:4], c4);
+   and(g0p1, g0, p1);
+   and(cp01, as, p0, p1);
+   or(c8, g1, g0p1, cp01);
+   cla4 CLA3(out[11:8], cout2, g2, p2, in1[11:8], in2m[11:8], c8);
+   and(cp02, as, p0, p1, p2);
+   and(g1p2, g1, p2);
+   and(g0p12, g0, p1, p2);
+   or(c12, g2, g1p2, g0p12, cp02);
+   cla4 CLA4(out[15:12], cout3, g3, p3, in1[15:12], in2m[15:12], c12);
+   and(cp03, as, p0, p1, p2, p3);
+   and(g2p3, g2, p3);
+   and(g1p23, g1, p2, p3);
+   and(g0p13, g0, p1, p2, p3);
+   or(cout, g3, g2p3, g1p23, g0p13, cp03);
+endmodule
 
 /**           ___           ___           ___           ___ 
  *      _    |   |    _    |   |    _    |   |    _    |   |
@@ -55,7 +109,7 @@ endmodule // cla4
  * --->| |-->| L |-->| |-->| L |-->| |-->| L |-->| |-->| L |-->
  *     |_|   | A |   |_|   | A |   |_|   | A |   |_|   | A |
  *     ISB   |___|   ISB   |___|   ISB   |___|   ISB   |___|
- */
+ 
 module adder16(out, cout, in1, in2, as, clk);
    output [15:0] out;
    output 	 cout;
@@ -122,3 +176,4 @@ module adder16(out, cout, in1, in2, as, clk);
      end
    
 endmodule // adder16
+*/
